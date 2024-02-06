89c27c51408bdc98941c24e4934807ee2c1804da
connbrack <connor.brackley@mail.concordia.ca>
2024-02-05 21:27:22 -0500

    initial commit

---
+++ TinderNav.py
@@ -0,0 +1,66 @@
+import time
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class TinderNav:
+    def __init__(self, driver=None):
+        """
+        Initializes an instance of TinderNav.
+        """
+        self.driver = driver
+
+    def createDriver(self):
+        """
+        Creates and returns a new Chrome webdriver with specified options.
+        """
+        options = uc.ChromeOptions()
+        options.headless = False
+        options.add_argument("--enable-geolocation")
+        options.add_argument("--user-data-dir=./browser-data")
+        driver = uc.Chrome(options=options)
+        self.driver = driver
+
+    def login(self):
+        """
+        Manages the login process. If already logged in, notifies the user. 
+        Otherwise, prompts user to manually log in.
+        """
+        time.sleep(3)
+        if self.driver.current_url == 'https://tinder.com/app/recs':
+            print('looks like you are already logged in!')
+        else:
+            input("Please manually login, press enter when login is complete..")
+
+    def load_webpage(self):
+        """
+        Loads the main Tinder app webpage.
+        """
+        url = "https://tinder.com/app/"
+        self.driver.get(url)
+
+    def wait_for_like_button(self, wait_for_reload_time=60):
+        WebDriverWait(self.driver, wait_for_reload_time).until(EC.element_to_be_clickable((By.XPATH, "//button[span/span/span[text()='Like']]")))
+
+    def like(self):
+        """
+        Finds and clicks the 'Like' button on Tinder.
+        """
+        button = self.driver.find_element(
+            By.XPATH, "//button[span/span/span[text()='Like']]")
+        button.click()
+
+    def dislike(self):
+        """
+        Finds and clicks the 'Nope' button on Tinder.
+        """
+        button = self.driver.find_element(
+            By.XPATH, "//button[span/span/span[text()='Nope']]")
+        button.click()
+
+    def quit(self):
+        """
+        Closes the webdriver and quits the browser.
+        """
+        self.driver.quit()
---
+++ app.py
@@ -0,0 +1,49 @@
+import random
+import time
+from TinderNav import TinderNav
+
+# parameters
+chance_to_like = .93
+min_wait_time = .5
+max_wait_time = 2
+wait_for_reload_time = 240
+
+
+def main():
+    err_count = 0
+
+    tbot = TinderNav()
+    tbot.createDriver()
+    tbot.load_webpage()
+    tbot.login()
+
+    print('======== Running, use ctrl+c to quit  ========')
+
+    try:
+        while True:
+            time.sleep(random.uniform(min_wait_time, max_wait_time))
+            tbot.wait_for_like_button(wait_for_reload_time)
+            try:
+                if random.random() < chance_to_like:
+                    tbot.like()
+                else:
+                    tbot.dislike()
+
+                err_count = 0
+                
+            except Exception:
+                if err_count >= 2:
+                    break
+                else:
+                    err_count += 1
+                    tbot.load_webpage()
+
+    except KeyboardInterrupt:
+        pass
+        
+    input('\nsomething went wrong :( ... press enter to quit')
+    tbot.quit()
+        
+
+if __name__ == "__main__":
+    main()
