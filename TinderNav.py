338eac9917b51f4a1ba1a3f2dab1c3923d53ee53
Connor Brackley <noreply@git.com>
2024-10-06 17:55:25 -0400

    Reformatted spacing

--- TinderNav.py
+++ TinderNav.py
@@ -5,66 +5,68 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class TinderNav:
-    def __init__(self, driver=None):
-        """
-        Initializes an instance of TinderNav.
-        """
-        self.driver = driver
+  def __init__(self, driver=None):
+    """
+    Initializes an instance of TinderNav.
+    """
+    self.driver = driver
 
-    def createDriver(self):
-        """
-        Creates and returns a new Chrome webdriver with specified options.
-        """
-        file_dir = os.path.dirname(__file__)
-        data_dir = os.path.join(file_dir, 'browser-data')
+  def createDriver(self):
+    """
+    Creates and returns a new Chrome webdriver with specified options.
+    """
+    file_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(file_dir, 'browser-data')
 
-        options = uc.ChromeOptions()
-        options.headless = False
-        options.add_argument("--enable-geolocation")
-        options.add_argument(f"--user-data-dir={data_dir}")
-        driver = uc.Chrome(options=options)
-        self.driver = driver
+    options = uc.ChromeOptions()
+    options.headless = False
+    options.add_argument("--enable-geolocation")
+    options.add_argument(f"--user-data-dir={data_dir}")
+    driver = uc.Chrome(options=options)
+    self.driver = driver
 
-    def login(self):
-        """
-        Manages the login process. If already logged in, notifies the user. 
-        Otherwise, prompts user to manually log in.
-        """
-        time.sleep(3)
-        if self.driver.current_url == 'https://tinder.com/app/recs':
-            print('looks like you are already logged in!')
-        else:
-            input("Please manually login, press enter when login is complete..")
+  def login(self):
+    """
+    Manages the login process. If already logged in, notifies the user. 
+    Otherwise, prompts user to manually log in.
+    """
+    time.sleep(3)
+    if self.driver.current_url == 'https://tinder.com/app/recs':
+      print('looks like you are already logged in!')
+    else:
+      input("Please manually login, press enter when login is complete..")
 
-    def load_webpage(self):
-        """
-        Loads the main Tinder app webpage.
-        """
-        url = "https://tinder.com/app/"
-        self.driver.get(url)
+  def load_webpage(self):
+    """
+    Loads the main Tinder app webpage.
+    """
+    url = "https://tinder.com/app/"
+    self.driver.get(url)
 
-    def wait_for_like_button(self, wait_for_reload_time=60):
-        WebDriverWait(self.driver, wait_for_reload_time).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Like']]")))
+  def wait_for_like_button(self, wait_for_reload_time=60):
+    WebDriverWait(self.driver, wait_for_reload_time).until(EC.element_to_be_clickable(
+        (By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Like']]")))
 
-    def like(self):
-        """
-        Finds and clicks the 'Like' button on Tinder.
-        """
-        button = self.driver.find_element(
-            By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Like']]")
-        button.click()
+  def like(self):
+    """
+    Finds and clicks the 'Like' button on Tinder.
+    """
+    button = self.driver.find_element(
+        By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Like']]")
+    button.click()
 
-    def dislike(self):
-        """
-        Finds and clicks the 'Nope' button on Tinder.
-        """
-        button = self.driver.find_element(
-            By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Nope']]")
-        button.click()
+  def dislike(self):
+    """
+    Finds and clicks the 'Nope' button on Tinder.
+    """
+    button = self.driver.find_element(
+        By.XPATH, "//button[contains(@class, 'button') and .//span[text()='Nope']]")
+    button.click()
 
-    def quit(self):
-        """
-        Closes the webdriver and quits the browser.
-        """
-        self.driver.quit()
+  def quit(self):
+    """
+    Closes the webdriver and quits the browser.
+    """
+    self.driver.quit()
--- app.py
+++ app.py
@@ -10,40 +10,40 @@ max_wait_for_like_button = 240
 
 
 def main():
-    err_count = 0
+  err_count = 0
 
-    tbot = TinderNav()
-    tbot.createDriver()
-    tbot.load_webpage()
-    tbot.login()
+  tbot = TinderNav()
+  tbot.createDriver()
+  tbot.load_webpage()
+  tbot.login()
 
-    print('======== Running, use ctrl+c to quit  ========')
+  print('======== Running, use ctrl+c to quit  ========')
 
-    try:
-        while True:
-            time.sleep(random.uniform(min_wait_time, max_wait_time))
-            tbot.wait_for_like_button(max_wait_for_like_button)
-            try:
-                if random.random() < chance_to_like:
-                    tbot.like()
-                else:
-                    tbot.dislike()
+  try:
+    while True:
+      time.sleep(random.uniform(min_wait_time, max_wait_time))
+      tbot.wait_for_like_button(max_wait_for_like_button)
+      try:
+        if random.random() < chance_to_like:
+          tbot.like()
+        else:
+          tbot.dislike()
 
-                err_count = 0
-                
-            except Exception:
-                if err_count >= 2:
-                    break
-                else:
-                    err_count += 1
-                    tbot.load_webpage()
+        err_count = 0
 
-    except KeyboardInterrupt:
-        pass
-        
-    input('\nsomething went wrong :( ... press enter to quit')
-    tbot.quit()
-        
+      except Exception:
+        if err_count >= 2:
+          break
+        else:
+          err_count += 1
+          tbot.load_webpage()
+
+  except KeyboardInterrupt:
+    pass
+
+  input('\nsomething went wrong :( ... press enter to quit')
+  tbot.quit()
+
 
 if __name__ == "__main__":
-    main()
+  main()
